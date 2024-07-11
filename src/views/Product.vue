9843e8cdf96d5dfa5d59927c24640a775df34afc
rimbasahara <rimbasahara76@gmail.com>
2024-07-12 01:00:45 +0700

    firstcommit

---
+++ src/views/Product.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="product">
+    <HeaderShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "ProductView",
+  components: {
+    HeaderShayna,
+    FooterShayna,
+  },
+};
+</script>
